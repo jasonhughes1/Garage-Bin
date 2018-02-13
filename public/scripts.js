523c268fa7aa9524e8bdc2c3e253f03bb601e2e2
Jason Hughes <jasonhughes088@gmail.com>
2018-02-12 17:24:04 -0700

    Update details and working on door opening

--- public/scripts.js
+++ public/scripts.js
@@ -19,7 +19,7 @@ const appendItems = (items) => {
       <div id=${item.id} class='item ${item.id} item ${item.cleanliness}'>
         <h4 class='item-name'>Item: ${item.name}</h4>
         <button class='details-btn'>Item Details</button>
-        <div class='hide-details'>
+        <div class='details hidden'>
           <h4 class='reason'>Reason: ${item.reason}</h4>
           <h4 class='condition'>Cleanliness: ${item.cleanliness}</h4>
           <select class="details-drop-down" name="">
@@ -32,6 +32,26 @@ const appendItems = (items) => {
     })
     counter();
   };
+
+  const details = (event) => {
+    console.log('fired');
+    $(event.target).siblings('.details').toggleClass('hidden');
+  }
+
+  const updateCleanliness = (event) => {
+  const updatedCleanliness = JSON.stringify({
+    cleanliness: event.target.value
+  });
+  const id = $(event.target).closest('.item').attr('id');
+
+  fetch(`/api/v1/items/${id}`, {
+    method: 'PATCH',
+    body: updatedCleanliness,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
 
 const orderItems = (items) => {
   console.log(items);
@@ -126,6 +146,8 @@ const counter = () => {
 
 
 $(document).ready(fetchItems)
+$('.garage').on('change', '.details-drop-down', (event) => updateCleanliness(event))
+$('.garage').on('click', '.details-btn', (event) => details(event));
 $('.submit-btn').on('click', addItem);
 $('.sort-btn').on('click', sortGarageItems)
 $('.open-btn').on('click', doorControl);
